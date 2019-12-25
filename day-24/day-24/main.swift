79ee42d1105907810ca3f3fa51f67f1278313379
Vladimir Svidersky <svidersky@gmail.com>
2019-12-25 13:56:25 -0800

    Day 24 - Part 2

--- day-24/day-24/main.swift
+++ day-24/day-24/main.swift
@@ -14,7 +14,12 @@ let size = 5
 func printOut(d: [Int]) {
     var line = ""
     for i in 0..<d.count {
-        line.append(d[i] > 0 ? "#" : ".")
+        if i == 12 {
+            line.append("?")
+        }
+        else {
+            line.append(d[i] > 0 ? "#" : ".")
+        }
         if line.lengthOfBytes(using: .utf8) == size {
             print(line)
             line = ""
@@ -75,6 +80,157 @@ func next(d: [Int]) -> [Int] {
     return result
 }
 
+func bugCount(d: [Int]) -> Int {
+    var bugCount = 0
+    for i in d {
+        if i > 0 {
+            bugCount += 1
+        }
+    }
+    return bugCount
+}
+
+func bottomLine(level: [Int]?) -> Int {
+    guard level != nil else { return 0 }
+    return level![20] + level![21] + level![22] + level![23] + level![24]
+}
+
+func topLine(level: [Int]?) -> Int {
+    guard level != nil else { return 0 }
+    return level![0] + level![1] + level![2] + level![3] + level![4]
+}
+
+func rightColumn(level: [Int]?) -> Int {
+    guard level != nil else { return 0 }
+    return level![4] + level![9] + level![14] + level![19] + level![24]
+}
+
+func leftColumn(level: [Int]?) -> Int {
+    guard level != nil else { return 0 }
+    return level![0] + level![5] + level![10] + level![15] + level![20]
+}
+
+func next2(level: [Int], prev: [Int]?, next: [Int]?) -> [Int] {
+    var result = [Int]()
+    
+    for i in 0..<level.count {
+        var nb = 0
+        
+        // Skip middle point
+        if i == 12 {
+            result.append(0)
+            continue
+        }
+        
+        // ^^^^^ Add neighbors from the top
+        // First row
+        if i < size && prev != nil {
+            nb += prev![7]
+        }
+        else if i >= size {
+            if i == 17 {
+                nb += bottomLine(level: next)
+            }
+            else {
+                nb += level[i - size]
+            }
+        }
+        
+        // vvvvv Add neighbors from the bottom
+        // Last row
+        if i >= (level.count-size) && prev != nil {
+            nb += prev![17]
+        }
+        else if i < (level.count-size) {
+            if i == 7 {
+                nb += topLine(level: next)
+            }
+            else {
+                nb += level[i+size]
+            }
+        }
+        
+        // <<<<< Add neigbors from the left
+        // First column
+        if (i % size) == 0 && prev != nil {
+            nb += prev![11]
+        }
+        else if (i % size) != 0 {
+            if i == 13 {
+                nb += rightColumn(level: next)
+            }
+            else {
+                nb += level[i-1]
+            }
+        }
+        
+        // >>>>> Add neighbors from the right
+        // Last colum
+        if ((i % size) == (size-1)) && prev != nil {
+            nb += prev![13]
+        }
+        else if (i % size) != (size-1) {
+            if i == 11 {
+                nb += leftColumn(level: next)
+            }
+            else {
+                nb += level[i+1]
+            }
+        }
+        
+        if level[i] == 0 && (nb == 1 || nb == 2) {
+            result.append(1)
+        }
+        else if level[i] == 1 && nb != 1 {
+            result.append(0)
+        }
+        else {
+            result.append(level[i])
+        }
+    }
+    
+    return result
+}
+
+// Part 2
+var startLevel = -1
+var levels = [empty, input, empty]
+
+for step in 1...200 {
+    
+    // Generate levels after next minute
+    var newLevels = [[Int]]()
+    for i in 0..<levels.count {
+        newLevels.append(
+            next2(level: levels[i], prev: (i > 0) ? levels[i-1] : nil, next: (i < levels.count-1) ? levels[i+1] : nil))
+    }
+    if bugCount(d: newLevels[0]) > 0 {
+        newLevels.insert(empty, at: 0)
+        startLevel -= 1
+    }
+    if bugCount(d: newLevels.last!) > 0 {
+        newLevels.append(empty)
+    }
+    
+    levels = newLevels
+    print("After \(step) min:")
+
+    for i in 0..<levels.count {
+        print("Level: \(startLevel+i)")
+        printOut(d: levels[i])
+    }
+}
+
+var bugs = 0
+for l in levels {
+    bugs += bugCount(d: l)
+}
+print("Total Bugs Count: \(bugs)")
+
+
+exit(0)
+
+// Part 1
 var check = [String:Bool]()
 
 check[hash(d: input)] = true
